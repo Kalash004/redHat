116872d1e25b1bc21a08bb934a65d5e612500044
Anton Kalashnikov <kalashnikov@SPSEJECNANET.CZ>
2022-11-07 08:52:28 +0100

    bruh am dead fr

---
+++ redHat/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using redHat;
+namespace red
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Game game = new Game();
+        }
+    }
+
+}
+
