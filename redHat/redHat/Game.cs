ec4f27d6bff93092f2dd217037fd94e5c7da03eb
Anton Kalashnikov <59295791+Kalash004@users.noreply.github.com>
2022-11-07 08:55:02 +0100

    broke init
    sad

---
+++ redHat/redHat/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redHat
+{
+    internal class Game
+    {
+        private bool playing = false;
+        private Map map = null;
+        public Game()
+        {
+            map = new Map();
+            Console.WriteLine("map created");
+            Console.WriteLine(map.ToString());
+        }
+
+        public void GameLoop()
+        {
+            // start game
+            while (playing)
+            {
+                // game loop
+            }
+            // end game 
+        }
+    }
+}
