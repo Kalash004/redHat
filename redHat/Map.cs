415125042dbc8b2ffea1572795e90b9f024eab3e
Anton <kalashnikov004@gmail.com>
2022-11-12 20:39:24 +0100

    squares

--- redHat/Map.cs
+++ redHat/Map.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace redHat
 {
+    public enum SquareTypes
+    {
+        Trap, // asks a question, if you dont answer you lose 1 item from basket
+        Wolf, // starts to go to the granny after 2 moves (if you need to obtain some items for granny he will be faster?)
+        GrannyHouse, // end
+        RedHatHouse, // start
+        Hill, // can check surrounding places 2 squares around
+        Hole, // transports to random position
+        MushroomField, // you can get some stuff for your granny
+        Hunters, // if you find them and there is a wolf hunting granny, they will hunt him down
+        Lake, // you cant pass this
+    }
     internal class Map
     {
-        private int X = 5, Y = 5; // current map dimensions 
-        private Square[,] game_map;
+        protected int X = 5, Y = 5; // current map dimensions 
+        protected Square[,] game_map;
+        protected Square player_pos;
         public Map()
         {
             this.game_map = GenerateMap();
@@ -20,14 +34,64 @@ namespace redHat
         // maybe in future game hardness (bigger map ?)
         public Square[,] GenerateMap()
         {
-            Square[,] map_generation = new Square[X,Y];
+            Square[,] map_generation = new Square[X, Y];
             Console.WriteLine("Generating map");
             for (int i = 0; i < X; i++)
             {
                 for (int j = 0; j < Y; j++)
                 {
+                    Random rand = new Random();
+                    int val = rand.Next(0, 9); // maybe add some posibility of different squares
+                    switch (val)
+                    {
+                        case 0: // lake
+                            Square sqr = new Lake();
+                            map_generation[i, j] = sqr;
+
+                            break;
+
+                        case 1: // trap
+                            map_generation[i, j] = sqr;
+
+                            break;
+
+                        case 2: // wolf
+                            map_generation[i, j] = sqr;
+
+                            break;
+
+                        case 3: // grannys house
+                            map_generation[i, j] = sqr;
+
+                            break;
+
+                        case 4: // red hats house
+                            map_generation[i, j] = sqr;
+
+                            break;
+
+                        case 5: // hill 
+                            map_generation[i, j] = sqr;
+
+                            break;
+
+                        case 6: // hole
+                            map_generation[i, j] = sqr;
+
+                            break;
+
+                        case 7: // field
+                            map_generation[i, j] = sqr;
+
+                            break;
+
+                        case 8: // hunters
+                            map_generation[i, j] = sqr;
+
+                            break;
+
+                    }
                     // generate column values(add level auto generations [traps and other stuff]);
-                    Square sqr = new Square(i.ToString()+j.ToString(), "f", "f");
                     map_generation[i, j] = sqr;
                 }
             }
--- redHat/Squares/Square.cs
+++ redHat/Squares/Square.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace redHat
+namespace redHat.Squares
 {
     /* 
      * WAYS TO GET THRU
@@ -14,27 +14,30 @@ namespace redHat
     internal class Square
     {
         private int hard; // TODO: maybe implement hardcorness levels 
-        private String name;
-        private String problem; // TODO: description / question to pass this location
-        private String answer; // TODO: answer to problem 
+        private string name;
+        private string problem; // TODO: description / question to pass this location
+        private string answer; // TODO: answer to problem 
+        private bool playerOn;
+        private bool seen = false;
         public Square()
         {
 
         }
 
-        public Square(string name, string problem, string answer)
+        public Square(string name, string problem, string answer, bool playerOn)
         {
             this.name = name;
             this.problem = problem;
             this.answer = answer;
+            this.playerOn = playerOn;
         }
 
-        public virtual String GenerateQuestionAnswer()
+        public virtual string GenerateQuestionAnswer()
         {
-            throw new NotImplementedException();  
+            throw new NotImplementedException();
         }
 
-        public virtual bool Pass(String ans)
+        public virtual bool Pass(string ans)
         {
             // TODO: Implement testing for answer / or number fight (choose a number if number is bigger than number from square and is not +3 bigger pass else stuck) 
             //if answered make it to give move options from Map
@@ -47,7 +50,18 @@ namespace redHat
 
         public override string ToString()
         {
-            return name.ToString();
+            if (playerOn)
+            {
+                return "-o-";
+            }
+            else if (seen)
+            {
+                return name.ToString();
+            }
+            else
+            {
+                return "?";
+            }
             /*
             return String.Format("Square name: {0}" +
                 "Problem: {1} \n" +
